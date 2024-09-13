1d5580dbfbbd32e1e06cd86318ed5cb6496cf919
Santos2175 <santoshgurung949@gmail.com>
2024-09-13 13:48:38 +0545

    recommended section added

--- src/Recommended/Recommended.jsx
+++ src/Recommended/Recommended.jsx
@@ -1,7 +1,21 @@
+import Button from '../components/Button';
 import './Recommended.css';
 
-const Recommended = () => {
-  return <div>Recommended</div>;
+const Recommended = ({ handleClick }) => {
+  return (
+    <>
+      <div>
+        <h2 className='recommended-title'>Recommended</h2>
+        <div className='recommended-flex'>
+          <Button onClickHandler={handleClick} title='All Products' />
+          <Button onClickHandler={handleClick} title='Nike' />
+          <Button onClickHandler={handleClick} title='Adidas' />
+          <Button onClickHandler={handleClick} title='Puma' />
+          <Button onClickHandler={handleClick} title='Vans' />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Recommended;
